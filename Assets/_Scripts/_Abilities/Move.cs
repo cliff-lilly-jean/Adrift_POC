7c99f82acc38f0592d339ba3f6d405ddc06848f3
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2024-07-28 22:09:28 -0400

    upates to the movement system; added the input system and got the character to move

--- Assets/_Scripts/_Abilities/Move.cs
+++ Assets/_Scripts/_Abilities/Move.cs
@@ -20,5 +20,42 @@ public class Move : Ability {
     public override void Use()
     {
         // Null
+        GetMoveDirection();
+    }
+
+    // * test method implementation
+
+    public Vector2 GetMoveDirection() {
+
+        // gets the direction to the vector2 coming from the Input Reader/Controller
+        Debug.Log("Moving from the move class");
+       return  _direction.value;
+    }
+
+    public void ResetMoveDirection() {
+
+        // zero out the direction so that the deceleration method can run to stop the movement
+        _direction.value = Vector2.zero;
+    }
+
+    public void Accelerate(Rigidbody2D _rb) {
+
+        // Add force  to the rigidbody in the movement direction multiplied by the acceleration to speed it up
+        _rb.AddForce(_direction.value * _acceleration.value * _force.value, ForceMode2D.Force);
+
+        // If the velocity is higher than the max speed, zero it out and multiply by the max speed
+        if (_rb.velocity.magnitude > _maxSpeed.value) {
+            _rb.velocity = _rb.velocity.normalized * _maxSpeed.value;
+        }
+
+        // TODO: Cut the bottom code out into its own class, ex. "Vision"
+        RaycastHit2D hit = Physics2D.Raycast(_rb.position, _direction.value, 10f);
+        Debug.DrawRay(_rb.position, _direction.value , Color.green);
+    }
+
+     public void Decelerate(Rigidbody2D _rb) {
+
+        // Add force  to the rigidbody in the movement direction multiplied by the deceleration to slow it down
+        _rb.AddForce(_rb.velocity * -_deceleration.value * _force.value, ForceMode2D.Force);
     }
 }
--- Assets/_Scripts/_Controllers/Character/Player/PlayerController.cs
+++ Assets/_Scripts/_Controllers/Character/Player/PlayerController.cs
@@ -1,83 +1,95 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Reflection; // * NOTE: imported to use the PropertyInfo, FieldInfo, GetProperties() and GetFields()
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private MovementSystem _movementSystem;
+    [SerializeField] private GameControls _controls;
     private Rigidbody2D _rb;
 
-    // private Move _moveAbility;
-    // private Dash _dashAbility;
+    // Dictionary to hold the name of the ability; string and a Dictionary of each property as key value pairs; string, object
+    private Dictionary<Type, Ability> _abilitiesInfo = new Dictionary<Type, Ability>();
+
+
+    private void Awake() {
+        _controls = new GameControls();
+        _rb = GetComponentInChildren<Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-       // Get all the abilities in the movement system
-        foreach (Ability ability in _movementSystem.movementAbilities)
-        {
-            // Check if the ability is of type Move
-            if (ability is Move moveAbility)
-            {
-                Debug.Log("Moving");
-                Debug.Log(moveAbility.description.value);
-                Debug.Log(moveAbility._maxSpeed.value);
-            }
 
-            // Check if the ability is of type Dash
-            if (ability is Dash dashAbility)
-            {
-                Debug.Log("Dashing");
-                Debug.Log(dashAbility.Name.value);
-                Debug.Log(dashAbility.dashSpeed.value);
-            }
+        InitializeAbilities();
 
-            // !NOTE: Possibly use a switch statement to cycle through the abilities
+        // *! Possible implementation??? GetAllAbilities();
+    }
+
+     private void OnEnable()
+    {
+        _controls.Enable();
+
+        _controls.Player.Move.performed += _ => GetMoveAbility().GetMoveDirection();
+        _controls.Player.Move.canceled += _ => GetMoveAbility().ResetMoveDirection();
+
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+
+        _controls.Player.Move.performed -= _ => GetMoveAbility().GetMoveDirection();
+        _controls.Player.Move.canceled -= _ => GetMoveAbility().ResetMoveDirection();
+
+    }
+
+    private void FixedUpdate()
+    {
+        //
+        if (GetMoveAbility()._direction.value == null) return;
+
+        // connect the controls to the direction of the Vector2
+        GetMoveAbility()._direction.value = _controls.Player.Move.ReadValue<Vector2>();
+
+        // checking the direction value is not 0
+        if(GetMoveAbility()._direction.value != Vector2.zero) {
+            GetMoveAbility().Accelerate(_rb);
+        }else {
+            GetMoveAbility().Decelerate(_rb);
         }
     }
 
-    // private void FixedUpdate()
-    // {
-    //     if (_movementSystem.move._direction.value == null) return;
+    // Ability Setup
+    private void InitializeAbilities()
+    {
+        foreach (Ability ability in _movementSystem.movementAbilities)
+        {
+            _abilitiesInfo[ability.GetType()] = ability;
+        }
+    }
 
-    //     // checking the direction value
-    //     if(_movementSystem.move._direction.value != Vector2.zero) {
-    //         Accelerate();
-    //     }else {
-    //         Decelerate();
-    //     }
-    // }
 
-    // private void GetMoveDirection(Vector2 direction) {
+    // Get the ability from the abilities list
+    private T GetAbility<T>() where T : Ability
+    {
+        _abilitiesInfo.TryGetValue(typeof(T), out Ability ability);
+        return ability as T;
+    }
 
-    //     // gets the direction to the vector2 coming from the Input Reader/Controller
-    //     _direction.value = direction;
-    // }
+    // Get Move Ability if there is one in the movement system
+    private Move GetMoveAbility()
+    {
+        var moveAbility = GetAbility<Move>();
+        if (moveAbility != null)
+        {
+            return moveAbility;
+            // Use moveAbility properties and methods
+        }
 
-    // private void ResetMoveDirection() {
+        return null;
+    }
 
-    //     // zero out the direction so that the deceleration method can run to stop the movement
-    //     _direction.value = Vector2.zero;
-    // }
 
-    // private void Accelerate() {
-
-    //     // Add force  to the rigidbody in the movement direction multiplied by the acceleration to speed it up
-    //     _rb.AddForce(_direction.value * _acceleration.value * _forceMultiplier.value, ForceMode2D.Force);
-
-    //     // If the velocity is higher than the max speed, zero it out and multiply by the max speed
-    //     if (_rb.velocity.magnitude > _maxSpeed.value) {
-    //         _rb.velocity = _rb.velocity.normalized * _maxSpeed.value;
-    //     }
-
-    //     // TODO: Cut the bottom code out into its own class, ex. "Vision"
-    //     RaycastHit2D hit = Physics2D.Raycast(_rb.position, _direction.value, 10f);
-    //     Debug.DrawRay(_rb.position, _direction.value , Color.green);
-    // }
-
-    //  private void Decelerate() {
-
-    //     // Add force  to the rigidbody in the movement direction multiplied by the deceleration to slow it down
-    //     _rb.AddForce(_rb.velocity * -_deceleration.value * _forceMultiplier.value, ForceMode2D.Force);
-    // }
 }
