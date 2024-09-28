cf005e5c948a3a2ae19b83a241234f84d7b54e04
cliff-lilly-jean <cliff.lilly.jean@gmail.com>
2024-09-28 17:40:15 -0400

    component restructure and healthsystem

--- Assets/Components/Combat.cs
+++ Assets/Components/Combat.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
+    public float damage;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Health>().ChangeHealth(-damage);
+        Debug.Log("CURRENT HEALTH: " + collision.gameObject.GetComponent<Health>().currentHealth);
+    }
 }
